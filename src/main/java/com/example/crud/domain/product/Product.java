e68d68987cf6391e6e98cc95f949bb7a71bd384d
Jessica Arf <jessicalealarf8@gmail.com>
2023-11-05 16:30:27 -0300

    post product method created

--- src/main/java/com/example/crud/domain/product/Product.java
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -19,6 +19,14 @@ private String id;
 
 private String name;
 
-private Number price_in_cents;
+private Integer price_in_cents;
+
+public Product(RequestProduct requestProduct){
+
+    this.name = requestProduct.name();
+    this.price_in_cents = requestProduct.price_in_cents();
+
+}
+
 
 }
