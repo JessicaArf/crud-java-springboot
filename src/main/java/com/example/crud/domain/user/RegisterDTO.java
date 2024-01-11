03a70d00ff0688d2fd15755da5ca88c5d0b02bf3
Jessica Arf <jessicalealarf8@gmail.com>
2024-01-11 20:39:47 -0300

    user register created

---
+++ src/main/java/com/example/crud/domain/user/RegisterDTO.java
@@ -0,0 +1,5 @@
+package com.example.crud.domain.user;
+
+public record RegisterDTO(String login, String password, UserRole role){
+
+}
--- src/main/java/com/example/crud/domain/user/User.java
+++ src/main/java/com/example/crud/domain/user/User.java
@@ -28,6 +28,13 @@ public class User implements UserDetails {
     private String password;
     private UserRole role;
 
+    public User(String login, String password, UserRole role){
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
 
