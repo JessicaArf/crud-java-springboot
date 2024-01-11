179d34cb3e629350e32f7e6708b1141b67057ec1
Jessica Arf <jessicalealarf8@gmail.com>
2024-01-10 21:16:01 -0300

    user repository created

--- src/main/java/com/example/crud/domain/user/User.java
+++ src/main/java/com/example/crud/domain/user/User.java
@@ -1,9 +1,19 @@
 package com.example.crud.domain.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.metamodel.internal.AbstractDynamicMapInstantiator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name="users")
 @Entity(name="users")
@@ -11,11 +21,47 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of="id")
-public class User {
+public class User implements UserDetails {
 
     private String id;
     private String login;
     private String password;
-    private String role;
+    private UserRole role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+         else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
